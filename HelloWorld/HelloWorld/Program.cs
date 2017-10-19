3c78ecdc4707637d4e9e51a5bd30eb559eef5a0c
icehap001 <icehap001@gmail.com>
2017-10-19 23:32:03 +0700

    ฟหก

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,6 +10,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Year 2558 semester 1,Test .");
+            Console.WriteLine("Year 2558 semester 2,Test .");
+            Console.WriteLine("Year 2559 semester 1,Test .");
+            Console.WriteLine("Year 2559 semester 2,Test .");
+            Console.WriteLine("Year 2560 semester 1, I am an Engedu freshman.");
+            Console.WriteLine("Year 2560 semester 2, I learn to program in C#.");
+            Console.WriteLine("Year 2561 semester 1, I Start my programer life.");
+            Console.WriteLine("Year 2561 semester 2, I Start my programer life.");
         }
     }
 }
